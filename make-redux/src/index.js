d683164699814ed452423a989055b00e0a343892
张明朝 <1104272319@qq.com>
2018-06-19 09:59:39 +0800

    make redux app2

--- make-redux/src/index.js
+++ make-redux/src/index.js
@@ -1,25 +1,12 @@
-let appState = {
-  title: {
-    text: 'React.js 小书',
-    color: 'red',
-  },
-  content: {
-    text: 'React.js 小书内容',
-    color: 'blue'
+function createStore (state, stateChanger) {
+  const listeners = []
+  const subscribe = (listener) => listeners.push(listener)
+  const getState = () => state
+  const dispatch = (action) => {
+    stateChanger(state, action)
+    listeners.forEach((listener) => listener())
   }
-}
-
-function dispatch (action) {
-  switch (action.type) {
-    case 'UPDATE_TITLE_TEXT':
-      appState.title.text = action.text
-      break
-    case 'UPDATE_TITLE_COLOR':
-      appState.title.color = action.color
-      break
-    default:
-      break
-  }
+  return { getState, dispatch, subscribe }
 }
 
 function renderApp (appState) {
@@ -39,7 +26,33 @@ function renderContent (content) {
   contentDOM.style.color = content.color
 }
 
-renderApp(appState) // 首次渲染页面
-dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》' }) // 修改标题文本
-dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'blue' }) // 修改标题颜色
-renderApp(appState) // 把新的数据渲染到页面上
+let appState = {
+  title: {
+    text: 'React.js 小书',
+    color: 'red',
+  },
+  content: {
+    text: 'React.js 小书内容',
+    color: 'blue'
+  }
+}
+
+function stateChanger (state, action) {
+  switch (action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      state.title.text = action.text
+      break
+    case 'UPDATE_TITLE_COLOR':
+      state.title.color = action.color
+      break
+    default:
+      break
+  }
+}
+
+const store = createStore(appState, stateChanger)
+store.subscribe(() => renderApp(store.getState())) // 监听数据变化
+
+renderApp(store.getState()) // 首次渲染页面
+store.dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》' }) // 修改标题文本
+store.dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'blue' }) // 修改标题颜色
