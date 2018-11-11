5bdfd0b8dc02579650536dc599323812de8b89e6
zhangmingchao8 <zhangmingchao8@jd.com>
2018-11-11 22:40:00 +0800

    react waterFall

---
+++ react-waterfall/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AutoResponsive from 'autoresponsive-react';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AutoResponsive> </AutoResponsive>
+      </div>
+    );
+  }
+}
+
+export default App;
